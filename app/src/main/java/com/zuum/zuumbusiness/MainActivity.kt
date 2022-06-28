972b847028a162a08489eb527998a90b279592a6
Javier Fernández <leqjl93@hotmail.com>
2022-06-27 21:15:55 -0500

    initial commit

---
+++ app/src/main/java/com/zuum/zuumbusiness/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.zuum.zuumbusiness
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.zuum.zuumbusiness.ui.main.SectionsPagerAdapter
+import com.zuum.zuumbusiness.databinding.ActivityMainBinding
+import com.zuum.zuumbusiness.ui.main.room.RoomFragment
+import com.zuum.zuumbusiness.ui.main.state.StateFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+
+        sectionsPagerAdapter.addFragment(RoomFragment(),"Rooms")
+        sectionsPagerAdapter.addFragment(StateFragment(),"States")
+        // sectionsPagerAdapter.addFragment(RoomFragment,"Rooms")
+
+        viewPager.adapter = sectionsPagerAdapter
+
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        // setUpTabs()
+    }
+    private fun setUpTabs()
+    {
+        // val adapter = ViewPagerAdapter(supportFragmentManager)
+
+    }
+}
---
+++ app/src/main/java/com/zuum/zuumbusiness/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,44 @@
+package com.zuum.zuumbusiness.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.zuum.zuumbusiness.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmenTitleList = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        return mFragmentList[position]
+        // return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mFragmenTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String)
+    {
+        mFragmentList.add(fragment)
+        mFragmenTitleList.add(title)
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+}
